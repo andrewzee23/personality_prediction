2109767ead31462f1f5364b8272e9761c1da6b48
andrewzee23 <andrewazamora@gmail.com>
2021-01-13 14:52:40 -0600

    added routes to app.py and created main.css file

--- andrew/app.py
+++ andrew/app.py
@@ -3,11 +3,25 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/homepage')
 def home():
 
     return None
 
+@app.route('/data1')
+def data1():
+
+    return None
+
+@app.route('/data2')
+def data2():
+
+    return None
+
+@app.route('/data3')
+def data3():
+
+    return None
 
 if __name__ == "__main__":
     app.run(debug=True)
